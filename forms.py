2678da09f3dc47c350ba2bf0b8f38799d223838c
AmmarMandsourwala <amandsourwala@gmail.com>
2025-05-10 20:39:47 +0530

    email validation

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 class Registrationform(FlaskForm):
     username=StringField("Username",validators=[DataRequired(),Length(min=2,max=20)])   #by datarequired it can't be empty and length will be used as given
-    email=StringField("Email",validators=[DataRequired(),Email()])
+    email=StringField('Email',validators=[DataRequired(),Email()])
     password=PasswordField("Password",validators=[DataRequired()])
     confirm_password=PasswordField("Confirm password",validators=[DataRequired(),EqualTo("password")])     # to equate password and confirm password equalTo is used
     submit=SubmitField("Sign Up")
